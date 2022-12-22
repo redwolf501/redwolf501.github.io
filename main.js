044e65c6480bf8fad3bcbf2dfaf21a9d38aeaa35
redwolf501 <112819447+redwolf501@users.noreply.github.com>
2022-12-22 13:11:03 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // 当网页向下滑动 40px 出现"返回顶部" 按钮
-window.onscroll = function() {scrollFunction()};
+
+/*window.onscroll = function() {scrollFunction()};
  
 function scrollFunction() {console.log(121);
     if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
@@ -8,7 +9,7 @@ function scrollFunction() {console.log(121);
         document.getElementById("tool").style.display = "none";
     }
 }
- 
+*/
 // 点击按钮，返回顶部
 function totop() {
     document.body.scrollTop = 0;
